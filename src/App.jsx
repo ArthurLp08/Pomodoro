e84282773197965647d44f38d28f8ccb0945d716
ArthurLp08 <josearthurlda@gmail.com>
2026-01-29 10:15:49 -0300

    timer is now displayed

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { useState, useEffect } from 'react'
 
 
 function App() {
+  const [timer, setTimer] = useState(localStorage.getItem("pomodoro"));
+  const [seconds, setSeconds] = useState(timer * 60);
+  const [mode, setMode] = useState("pomo"); //pomo, short, long
 
   useEffect(() => {
-    let storage = localStorage.getItem("pomodoro");
+    let storage = localStorage.getItem("enteredBefore");
     if (!storage){
       localStorage.setItem("pomodoro", 25);
       localStorage.setItem("shortBreak", 5);
       localStorage.setItem("LongBreak", 15);
+      localStorage.setItem("enteredBefore", true);
     }
-  })
+  }, []);
+
+  const formatTimeLeft = (seconds) => {
+    return(`${Math.floor(seconds/60)}:${
+      (seconds % 60 > 9)
+      ? seconds % 60
+      : '0' + seconds % 60
+    }`);
+  }
 
   return (
     <>
-    
+      <h1>{formatTimeLeft(seconds)}</h1>
     </>
   )
 }
