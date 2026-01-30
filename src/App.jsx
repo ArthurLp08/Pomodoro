a7726439898cae8ddf1c4429dea9e8b323560d8d
ArthurLp08 <josearthurlda@gmail.com>
2026-01-30 16:09:10 -0300

    Timer works now

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, act } from 'react'
 
 
 function App() {
   const [timer, setTimer] = useState(localStorage.getItem("pomodoro"));
   const [seconds, setSeconds] = useState(timer * 60);
   const [mode, setMode] = useState("pomo"); //pomo, short, long
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
     let storage = localStorage.getItem("enteredBefore");
@@ -19,14 +20,27 @@ function App() {
   const formatTimeLeft = (seconds) => {
     return(`${Math.floor(seconds/60)}:${
       (seconds % 60 > 9)
-      ? seconds % 60
+      ? seconds % 60 
       : '0' + seconds % 60
     }`);
+  }
+
+  useEffect(() => {
+    if (active){
+      setTimeout(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    }
+  }, [seconds, active]);
+
+  const HandleStart = () => {
+    setActive(true);
   }
 
   return (
     <>
       <h1>{formatTimeLeft(seconds)}</h1>
+      <button onClick={HandleStart}>Start</button>
     </>
   )
 }
